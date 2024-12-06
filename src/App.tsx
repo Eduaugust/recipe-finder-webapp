b75ab4b08614476ee2e695fc26f47f2ed6c64666
eduaugust <eduardo.evangelista2002@gmail.com>
2024-12-06 15:47:32 -0300

    :sparkles: feat: add dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import './App.css';
 import { Route, Routes, BrowserRouter as Router} from 'react-router';
 import RecipesPage from './pages/RecipesPage';
 import FavoritesPage from './pages/FavoritesPage';
+import { ConfigProvider, theme } from 'antd';
+import { useDarkMode } from './contexts/DarkModeContext';
 
 function App() {
+
+  const { isDarkMode } = useDarkMode();
+
+  console.log(isDarkMode);
+
   return (
+    <ConfigProvider theme={{ algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm }}>
       <Router>
         <Routes>
           <Route path="/" Component={RecipesPage} />
           <Route path="/favorites" Component={FavoritesPage} />
         </Routes>
       </Router>
+    </ConfigProvider>
   );
 }
 
--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row, Typography } from 'antd'
+import DarkModeButton from '../../components/DarkModeButton/inedx';
 
 const Header: React.FC = () => {
   return (
@@ -7,6 +8,9 @@ const Header: React.FC = () => {
       <Col span={16}>
         <Typography.Title level={2} className="text-3xl font-bold mb-2 ">Recipes Page</Typography.Title>
         <Typography.Paragraph className="text-lg">Discover and save your favorite recipes from a wide variety of cuisines</Typography.Paragraph>
+      </Col>
+      <Col span={8} className="flex mt-4 justify-end">
+        <DarkModeButton />
       </Col>
     </Row>
   );
